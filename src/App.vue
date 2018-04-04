4bb548c7bc84b9ef7355d00fefc2e1fcd30bc701
Paul Schoen <pschoen325@gmail.com>
2018-04-03 20:05:18 -0400

    Pushing for Spencer

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Nanum Gothic', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

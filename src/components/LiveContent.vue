5223b4b67fb87f5c2f7ac4eed1bd132d973ee8f5
Spencer Bouse <Orangesrb@gmail.com>
2018-04-26 19:28:18 -0400

    upload

--- src/components/LiveContent.vue
+++ src/components/LiveContent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='live-container'>
     <h1>Live</h1>
+    <iframe class='tickets' src="//ticketbud.com/events/439e4a70-f174-11e7-a239-3f71614bba7b/external"></iframe>
   </div>
 </template>
 
@@ -12,6 +13,12 @@ export default {
 
 <style lang='scss'>
   @import '../../src/assets/sass/now-ui-kit.scss';
+
+  .tickets{
+    background-color: white;
+    width: 90%;
+    height: 26em;
+  }
 
   .live-container{
     background: linear-gradient($purple-bg, $darkpurple-bg);
